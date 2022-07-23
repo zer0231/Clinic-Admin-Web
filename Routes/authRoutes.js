b7562aee413165c912b55bc78c2e38b489ce04d1
Pranay Rajopadhyaya <pranay_zer0@protonmail.com>
2022-07-23 23:16:51 +0545

    used cookie-parser and dashboard+login page

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,7 +1,9 @@
 const {Router} = require('express');
+const baseController = require('../controllers/baseController');
 const homeController = require('../controllers/homeController');
 const loginController = require('../controllers/loginController');
 const dashboardController = require('../controllers/dashboardController');
+
 const router = Router();
 
 router.get('/',homeController.home_get);
@@ -10,5 +12,8 @@ router.post('/login',loginController.login_post);
 router.get('/reset-password',loginController.reset_password);
 
 router.get('/dashboard',dashboardController.dashboard_get);
+
+router.get('/logout',loginController.logout_get);
+router.all('*',baseController.check404);
 
 module.exports = router; //exporting routes
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = require('./Routes/authRoutes');
+const cookieParser = require('cookie-parser')
 const app = express();	
 const port = 2020;
+const {checkUser} = require('./middleware/auth');
 
 app.use(express.static('public'));
 
@@ -9,10 +11,18 @@ app.use(express.json()); //to understand the request received as json from POST
 	
 //setting view engine
 app.set('view engine','ejs');
+app.use(express.static('public'))
+//using cookie parser
+app.use(cookieParser())
 
 //listening server
 app.listen(port,
 	console.log("listening to port "+port));
+
+	// app.use('/*',(req,res,next)=>{
+		
+	// });
+
 
 //using routes
 app.use(routes);
